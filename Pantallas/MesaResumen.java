fbbbb94c8312e185ba2dfce260677eb119b741b9
erpepe13 <p3p30210@gmail.com>
2025-05-15 14:25:11 +0200

    Pantallas interfaces terminadas

---
+++ Pantallas/MesaResumen.java
@@ -0,0 +1,100 @@
+package Pantallas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.awt.Toolkit;
+
+public class MesaResumen extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MesaResumen frame = new MesaResumen();
+					frame.setLocationRelativeTo(null);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MesaResumen() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(MesaResumen.class.getResource("/Imagenes/Logotop.jpg")));
+		setTitle("Resumen de mesa");
+		ConexionBasedeDatos.ConexionMySQL x = new ConexionBasedeDatos.ConexionMySQL("root", "", "Registro");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 900, 700);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBackground(SystemColor.info);
+		textArea.setFont(new Font("Calibri", Font.BOLD, 28));
+		textArea.setBounds(203, 90, 489, 423);
+		contentPane.add(textArea);
+		textArea.setText(Restaurante.mensaje);
+		
+		JButton GenerarCuenta = new JButton("Generar Cuenta");
+		GenerarCuenta.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					x.conectar();
+					if(Restaurante.total==0) 
+					{
+						JOptionPane.showMessageDialog(null, "Mesa vacia o cobrada");
+					}
+					else 
+					{
+						Cobro c= new Cobro();
+						c.setVisible(true);
+						dispose();
+					}
+					
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+			}
+		});
+		GenerarCuenta.setBounds(344, 539, 197, 44);
+		contentPane.add(GenerarCuenta);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(MesaResumen.class.getResource("/Imagenes/marmolnegro (1).jpg")));
+		lblNewLabel.setBounds(0, 0, 884, 661);
+		contentPane.add(lblNewLabel);
+		
+		
+	}
+}
